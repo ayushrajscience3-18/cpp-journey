6fbcb136b2bccd4d4821f3c3217b14e973006caa
Ayush Raj <ayushrajscience3@gmail.com>
2026-03-01 06:18:17 +0530

    Day 2 Variables, comments and int sum

---
+++ tut3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+// This progrm was created by Ayush Raj
+ /* This 
+ is
+ Ayush Raj*/
+int main (){
+    int sum = 6;
+    cout<< "Hello World" << sum;
+    return 0;
+}
