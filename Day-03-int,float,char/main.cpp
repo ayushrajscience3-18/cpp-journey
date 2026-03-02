96e0fd30d0937be0f7f70f2bfed8b855d84b39b7
Ayush Raj <ayushrajscience3@gmail.com>
2026-03-02 07:44:07 +0530

    Day 03 of coding

---
+++ Day-03-int,float,char/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//int a = 4;
+ //int b = 5;
+int a=4, b=5;
+float pi=3.15;
+char c ='d';
+
+// \n means Enter
+
+cout<<"This is tutorial 4. \nHere the value of a is "<<a<<". The value of b is "<<b; 
+cout<<"The value of pi is: <<pi;
+cout<<"The value of c is: <<c;
+return 0;
+  }
