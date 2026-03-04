2b73bdd991b8500ea391631e40488601a9bd2f65
Ayush Raj <ayushrajscience3@gmail.com>
2026-03-04 08:15:52 +0530

    Day 5 of c++ coding
    Implement arithmetic operations and display results.

---
+++ Day-05-Arithmetic operators/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// Arithmetic operators
+
+int main(){
+  int a=4, b=5;
+  
+    cout<<"Operators in C++"<<endl;
+    // Arithmetic Operators
+  
+    cout<<"The value of a + b is "<<a+b<<endl;
+    cout<<"The value of a - b is "<<a-b<<endl;
+    cout<<"The value of a * b is "<<a*b<<endl;
+    cout<<"The value of a % b is "<<a%b<<endl;
+    cout<<"The value of a / b is "<<a/b<<endl;
+    cout<<"The value of a ++  is "<<a++<<endl;
+    cout<<"The value of a --  is "<<a++<<endl;
+    cout<<"The value of ++a is "<<++a<<endl;
+    cout<<"The value of --a is "<<++a<<endl;
+  return 0;
+}
