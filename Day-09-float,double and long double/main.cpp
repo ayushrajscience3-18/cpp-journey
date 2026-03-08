16c3d888cda121658563bb42389faff505d577bc
Ayush Raj <ayushrajscience3@gmail.com>
2026-03-08 08:20:49 +0530

    Day 9 of c++ coding
    Fixed variable declaration for long double and corrected output statements.

---
+++ Day-09-float,double and long double/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+  float d = 34.4F;
+long double = 34.4L;
+
+cout<<"The size of 34.4 is "<<size of (34.4)<<endl;
+cout<<"The size of 34.4f is "<<size of (34.4f)<<endl;
+cout<<"The size of 34.4F is "<<size of (34.4F)<<endl;
+cout<<"The size of 34.4l is "<<size of (34.4l)<<endl;
+cout<<"The size of 34.4L is "<<size of (34.4L)<<endl;
+
+cout<<"The size of d is"<<d<<endl<<"The value of e is"<<e;
+return 0;
+}
